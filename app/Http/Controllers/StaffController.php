83bba7344247d2d07044df0f48e629604ef2af69
Nguyễn Văn Ước <uocnv.soict.hust@gmail.com>
2021-09-29 13:33:17 +0700

    update redirect

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -10,7 +10,17 @@ class StaffController extends Controller
 {
     public function book(Staff $staff)
     {
-        Artisan::call('lunch:book', ['--user' => $staff->username]);
-        return redirect()->route('voyager.staff.index');
+        try {
+            Artisan::call('lunch:book', ['--user' => $staff->username]);
+        } catch (\Exception $exception) {
+            return redirect()->route('voyager.staff.index')->with([
+                'message'    => "Error when book for staff $staff->full_name",
+                'alert-type' => 'error',
+            ]);
+        }
+        return redirect()->route('voyager.staff.index')->with([
+            'message'    => "Successfully booked for staff $staff->full_name",
+            'alert-type' => 'success',
+        ]);
     }
 }
